a9706d3398f568fe4a4b623cd27eaaeb95630de0
David Gibbons <dgibbons999@gmail.com>
2019-07-24 23:21:48 +0100

    design

--- apps/display/admin.py
+++ apps/display/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Quote
 
+
 class QuoteAdmin(admin.ModelAdmin):
     list_display = ('text', 'author')
     list_display_links = ('text',)
--- apps/display/static/display/js/App.vue
+++ apps/display/static/display/js/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
+  <div class="full-area h-100">
     <div class="d-flex flex-row justify-content-center">
-      {{ quoteText }} - {{ quoteAuthor }}
+      <p>{{ quoteText }}</p>
     </div>
-    <div class="d-flex flex-row justify-content-center" @click="getQuote()">
-      Next
+    <div class="d-flex flex-row justify-content-center">
+      <p>- <i>{{ quoteAuthor }}</i></p>
+    </div>
+    <div class="d-flex flex-row justify-content-center">
+      <button class="btn btn-clear button" @click="getQuote()">
+        Next
+      </button>
     </div>
   </div>
 </template>
@@ -36,4 +41,23 @@ export default {
 };
 </script>
 <style>
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 35px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 30px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+.full-area {
+  background-color: #D3D3D3;
+  padding-top: 10%;
+}
+p {
+  font-size: 30px;
+}
 </style>
