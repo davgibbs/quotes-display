baf7957affe8a78497050e2b13e0702bd1c5f9c3
David Gibbons <dgibbons999@gmail.com>
2019-08-03 22:02:02 +0100

    Added webpack stats

--- apps/quotesdisplay/settings.py
+++ apps/quotesdisplay/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Webpack settings
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+    }
+}
