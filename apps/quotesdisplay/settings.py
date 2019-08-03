cc8c7a0f893c917e52ccb6211a1535a3e39ae8ba
David Gibbons <dgibbons999@gmail.com>
2019-08-03 22:20:26 +0100

    added

--- apps/quotesdisplay/settings.py
+++ apps/quotesdisplay/settings.py
@@ -121,6 +121,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # Django's collectstatic copies our bundles to the STATIC_ROOT or syncs them to whatever storage we use.
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# List of finder classes that know how to find static files in various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'display/static/display'),
 )
