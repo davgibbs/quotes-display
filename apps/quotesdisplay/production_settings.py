60af743f16d655c0e462b35261026170b6950aef
David Gibbons <dgibbons@eircom.net>
2019-09-03 15:50:44 +0100

    Added in another allowed host

--- apps/quotesdisplay/production_settings.py
+++ apps/quotesdisplay/production_settings.py
@@ -3,4 +3,4 @@ from .settings import *  # noqa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['davgibbs2.pythonanywhere.com']
+ALLOWED_HOSTS = ['davgibbs.pythonanywhere.com', 'davgibbs2.pythonanywhere.com']
