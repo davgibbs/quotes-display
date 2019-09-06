bee25eb670aa2fa0ee1f0b1465602a54d4829fd2
David Gibbons <dgibbons@eircom.net>
2019-09-06 12:38:03 +0100

    Added in the Babel config

--- webpack.common.js
+++ webpack.common.js
@@ -52,10 +52,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: file => (
-          /node_modules/.test(file) &&
-          !/\.vue\.js/.test(file)
-        )
+        exclude: /node_modules/,
       },
     ],
   },
