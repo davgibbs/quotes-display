1f4d7dbe8553d85760498702cd9876135c7ff477
David Gibbons <david.gibbons@openapp.ie>
2019-10-10 17:45:55 +0100

    Updated view

--- apps/quotes/views.py
+++ apps/quotes/views.py
@@ -1,5 +1,3 @@
-from random import randint
-
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
@@ -9,10 +7,10 @@ from .models import Quote
 
 def random_quote(request):
     """This method returns the quote text and the author"""
-    quote_ids = Quote.objects.all().values_list('id', flat=True)
-    random_index = randint(0, len(quote_ids) - 1)
-    quote = Quote.objects.get(pk=quote_ids[random_index])
+    quote = Quote.objects.order_by('?').first()
     return JsonResponse(model_to_dict(quote))
+
+
 
 
 def index(request):
